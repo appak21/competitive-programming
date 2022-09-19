c04dae9c1c5352662b906965bdda38b3db76037d
appak21 <zhadiko111@gmail.com>
2022-09-19 18:01:20 +0600

    update 2287

--- weekly-contest/295/2287. Rearrange Characters to Make Target String.go
+++ weekly-contest/295/2287. Rearrange Characters to Make Target String.go
@@ -1,5 +1,7 @@
 package contest
 
+import "strings"
+
 func rearrangeCharacters(s string, target string) int {
 	sch, tch := make([]int, 26), make([]int, 26)
 	for _, ch := range s {
@@ -17,21 +19,22 @@ func rearrangeCharacters(s string, target string) int {
 	return res
 }
 
+func rearrangeCharacters2(s string, target string) int {
+	res := len(s)
+	for _, ch := range target {
+		count := strings.Count(s, string(ch))
+		tcount := strings.Count(target, string(ch))
+		if tcount > count {
+			return 0
+		}
+		res = min(res, count/tcount)
+	}
+	return res
+}
+
 func min(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
 }
-
-/*
-#hex
-res := 0x3f3f3f3f // 3f3f3f3f = 1061109567
-– what is 0x  in programming ?
-– The prefix 0x is used in code to indicate that the number is being written in hex.
-Sometimes you need to set a large number. We can use math.MaxInt, however, we get overflow if we add the large number by 1.
-Programmers like to use the hex number 3f3f3f3f as INF.
-Reasons for that:
-0x3f3f3f3f is large enough in most cases
-0x3f3f3f3f + 0x3f3f3f3f will not overflow
-*/
